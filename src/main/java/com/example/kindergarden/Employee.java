6774f6b14088712fce3ebefa9ae166fad0a62157
JanniJensenDahm <tjukki1994@hotmail.com>
2018-03-06 22:56:34 +0100

    Added EmployeeList in employee.html Added Arraylist (FileHandler) + Constructor, Getters and Setters (Employee) not working

--- src/main/java/com/example/kindergarden/Employee.java
+++ src/main/java/com/example/kindergarden/Employee.java
@@ -1,7 +1,31 @@
 package com.example.kindergarden;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+
+    /*public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }*/
+
   public void addEmployeeToList(){
     System.out.println("kom ind");
   }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- src/main/java/com/example/kindergarden/FileHandler.java
+++ src/main/java/com/example/kindergarden/FileHandler.java
@@ -1,16 +1,26 @@
 package com.example.kindergarden;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileHandler {
     private String fileName;
     private Scanner scanner;
+    ArrayList<Employee> employees = new ArrayList<>();
 
     public FileHandler(String fileName) throws Exception {
         this.fileName = fileName;
         scanner = new Scanner(new File("./resources/"+fileName));
     }
+
+
+    /*public ArrayList<Employee> getEmployees(){
+        employees.add(new Employee(Janni, Jensen));
+
+        return employees;
+    }*/
+
 
     public String getFileName() {
         return fileName;
