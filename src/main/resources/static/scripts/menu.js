a2d1f270d6e29d8dd368b861028448315e7e4f39
JanniJensenDahm <tjukki1994@hotmail.com>
2018-03-16 11:52:47 +0100

    added edit password

--- src/main/resources/static/scripts/menu.js
+++ src/main/resources/static/scripts/menu.js
@@ -13,6 +13,38 @@ $(document).ready(function () {
         $("#menu ul li:nth-child(4) a").addClass("active");
 
     }
+    $("#openEditPassWord").click(function () {
+        $("#overlay, #editPassword").show();
+        $("#overlay-user, .popup-formular-user").hide();
+    });
+
+    $("#cancelEditPassWord, #overlay").click(function () {
+        $("#overlay, .popup-formular").hide();
+        $("#overlay-user, #user").show();
+    });
+    //Validation password
+    $(document).on('click', 'form input[name=savePassWord]', function(e) {
+        var isValid = true;
+        if ($("#currentPassword").val().length == 0) {
+            $(".currentPasswordFail").show();
+            isValid = false;
+        } else {
+            $(".currentPasswordFail").hide();
+        }
+        if ($("#newPassword").val().length == 0) {
+            $(".newPasswordFail").show();
+            isValid = false;
+        } else {
+            $(".newPasswordFail").hide();
+        }
+        if ($("#newPasswordCheck").val().length == 0) {
+            $(".passwordCheckFail").show();
+            isValid = false;
+        } else {
+            $(".passwordCheckFail").hide();
+        }
+        e.preventDefault();
+    });
 
     $("#openUserInfo").click(function (e) {
         $("#overlay-user, #user").show();
