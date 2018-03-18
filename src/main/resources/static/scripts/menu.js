99fb86de858c5ac4a1d7e9d16ecb7f01ac91414c
Mike Hemmje Jahn <mike@familien-jahn.dk>
2018-03-18 21:03:36 +0100

    ?

--- src/main/resources/static/scripts/menu.js
+++ src/main/resources/static/scripts/menu.js
@@ -1,64 +1,78 @@
 $(document).ready(function () {
-    var path = window.location.pathname;
-    if(path == "/index"){
-        $("#menu ul li:nth-child(1) a").addClass("active");
+  var path = window.location.pathname;
+  if (path == "/index") {
+    $("#menu ul li:nth-child(1) a").addClass("active");
 
-    }else if(path == "/employee"){
-        $("#menu ul li:nth-child(2) a").addClass("active");
+  } else if (path == "/employee") {
+    $("#menu ul li:nth-child(2) a").addClass("active");
 
-    }else if(path == "/children"){
-        $("#menu ul li:nth-child(3) a").addClass("active");
+  } else if (path == "/children") {
+    $("#menu ul li:nth-child(3) a").addClass("active");
 
-    }else if(path == "/waitingList"){
-        $("#menu ul li:nth-child(4) a").addClass("active");
+  } else if (path == "/waitingList") {
+    $("#menu ul li:nth-child(4) a").addClass("active");
 
+  }
+
+
+
+  $("#openEditPassWord").click(function () {
+    $("#editPassword").show();
+    $("#overlay").css("background", "rgba(0, 0, 0, 0.5)");
+    $("#user").hide();
+  });
+
+  $("#cancelUser, #cancelEditPassWord, #overlay").click(function () {
+
+console.log("pass vis: " + $("#editPassword").is(":visible"));
+console.log("user vis: " + $("#user").is(":visible"));
+    if($("#editPassword").is(":visible")) {
+      $("#editPassword").hide();
+      $("#overlay").css("background", "none");
+      $("#overlay, #user").show();
+      console.log(1);
     }
-    $("#openEditPassWord").click(function () {
-        $("#overlay-passWord, #editPassword").show();
-        $("#overlay-user, .popup-formular-user").hide();
-    });
+    else{
+      $("#overlay").css("background", "rgba(0, 0, 0, 0.5)");
+      $("#overlay, #user").hide();
+      $("#openUserInfo").removeClass('selected');
+      console.log(3);
+    }
 
-    $("#cancelEditPassWord, #overlay-passWord").click(function () {
-        $("#overlay-passWord, .popup-formular").hide();
-        $("#overlay-user, #user").show(); //denne skal lave om (finde en anden løsning)
-    });
-    //Validation password
-    $(document).on('click', 'form input[name=savePassWord]', function(e) {
-        var isValid = true;
-        if ($("#currentPassword").val().length == 0) {
-            $(".currentPasswordFail").show();
-            isValid = false;
-        } else {
-            $(".currentPasswordFail").hide();
-        }
-        if ($("#newPassword").val().length == 0) {
-            $(".newPasswordFail").show();
-            isValid = false;
-        } else {
-            $(".newPasswordFail").hide();
-        }
-        if ($("#newPasswordCheck").val().length == 0) {
-            $(".passwordCheckFail").show();
-            isValid = false;
-        } else {
-            $(".passwordCheckFail").hide();
-        }
-        e.preventDefault();
-    });
+  });
 
-    $("#openUserInfo").click(function (e) {
-        $("#overlay").css("background","none")
-        $("#overlay, #user").show();
-        $("#openUserInfo").removeClass('selected');
-        $(this).addClass('selected');
 
-        $("#userForm").attr("action", "saveUserInfo?path="+(window.location.pathname));
+  $("#openUserInfo").click(function () {
+    $("#overlay").css("background", "none");
+    $("#overlay, #user").show();
+    $(this).addClass('selected');
 
-        e.preventDefault();
-    });
+    $("#userForm").attr("action", "saveUserInfo?path=" + (window.location.pathname));
 
-    $("#cancelUser, #overlay").click(function () {
-        $("#overlay, .popup-formular-user").hide();
-        $("#openUserInfo").removeClass('selected');
-    });
+  });
+
+  //Validation password
+  $(document).on('click', 'form input[name=savePassWord]', function (e) {
+    var isValid = true;
+    if ($("#currentPassword").val().length == 0) {
+      $(".currentPasswordFail").show();
+      isValid = false;
+    } else {
+      $(".currentPasswordFail").hide();
+    }
+    if ($("#newPassword").val().length == 0) {
+      $(".newPasswordFail").show();
+      isValid = false;
+    } else {
+      $(".newPasswordFail").hide();
+    }
+    if ($("#newPasswordCheck").val().length == 0) {
+      $(".passwordCheckFail").show();
+      isValid = false;
+    } else {
+      $(".passwordCheckFail").hide();
+    }
+    e.preventDefault();
+  });
+
 });
