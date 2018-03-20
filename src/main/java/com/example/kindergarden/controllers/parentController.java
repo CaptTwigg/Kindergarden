3fc6f38b9eab7cb3095441aab566b0c228fd8236
capttwigg <mike@familien-jahn.dk>
2018-03-20 14:07:27 +0100

    update

--- src/main/java/com/example/kindergarden/controllers/parentController.java
+++ src/main/java/com/example/kindergarden/controllers/parentController.java
@@ -1,5 +1,6 @@
 package com.example.kindergarden.controllers;
 
+import com.example.kindergarden.FileHandler;
 import com.example.kindergarden.base.Child;
 import com.example.kindergarden.services.ServiceSession;
 import com.example.kindergarden.wrappers.ParentWrapper;
@@ -7,14 +8,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class parentController {
+  public FileHandler fileHandler = new FileHandler("parents.txt");
 
   @GetMapping("/parentList")
   public String parent(Model model){
     if(ServiceSession.isSomeoneLoggedIn()) {
+      model.addAttribute("niveau", ServiceSession.getCurrentSession().getUserNiveau());
       model.addAttribute("user", ServiceSession.getEmployeeDataForCurrentUser());
-      model.addAttribute("sessionUserName", ServiceSession.getCurrentSession().getUserName());
+      model.addAttribute("parents", fileHandler.loadAllParents());
+      model.addAttribute("parent", fileHandler.loadAllParents().get(0));
       return "parentList";
     }else{
       return "redirect:/";
